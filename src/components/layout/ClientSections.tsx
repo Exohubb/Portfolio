18abf875aacc66e6dc6a6342a7568e9505daf2d3
Exohubb <darkdevil3210123@gmail.com>
2026-03-06 09:34:20 +0530

    fix: correct organism import paths in ClientSections

--- src/components/layout/ClientSections.tsx
+++ src/components/layout/ClientSections.tsx
@@ -1,23 +1,23 @@
 'use client'
 import dynamic from 'next/dynamic'
 
-const HeroSection       = dynamic(() => import('@/components/organisms/HeroSection').then(m => m.HeroSection),             { ssr: false })
-const AboutSection      = dynamic(() => import('@/components/organisms/AboutSection').then(m => m.AboutSection),           { ssr: false })
-const NetworkSection    = dynamic(() => import('@/components/organisms/NetworkSection').then(m => m.NetworkSection),       { ssr: false })
-const ProjectsSection   = dynamic(() => import('@/components/organisms/ProjectsSection').then(m => m.ProjectsSection),     { ssr: false })
-const ExperienceSection = dynamic(() => import('@/components/organisms/ExperienceSection').then(m => m.ExperienceSection), { ssr: false })
-const CertsSection      = dynamic(() => import('@/components/organisms/CertsSection').then(m => m.CertsSection),           { ssr: false })
-const ContactSection    = dynamic(() => import('@/components/organisms/ContactSection').then(m => m.ContactSection),       { ssr: false })
+const HeroSection            = dynamic(() => import('@/components/organisms/HeroSection').then(m => m.HeroSection),                       { ssr: false })
+const AboutSection           = dynamic(() => import('@/components/organisms/AboutSection').then(m => m.AboutSection),                     { ssr: false })
+const NetworkTrackerSection  = dynamic(() => import('@/components/organisms/NetworkTrackerSection').then(m => m.NetworkTrackerSection),   { ssr: false })
+const ProjectsGrid           = dynamic(() => import('@/components/organisms/ProjectsGrid').then(m => m.ProjectsGrid),                     { ssr: false })
+const ExperienceSection      = dynamic(() => import('@/components/organisms/ExperienceSection').then(m => m.ExperienceSection),           { ssr: false })
+const CertificationsSection  = dynamic(() => import('@/components/organisms/CertificationsSection').then(m => m.CertificationsSection),   { ssr: false })
+const ContactSection         = dynamic(() => import('@/components/organisms/ContactSection').then(m => m.ContactSection),                 { ssr: false })
 
 export function ClientSections() {
   return (
     <>
       <section id="home"><HeroSection /></section>
       <section id="about"><AboutSection /></section>
-      <section id="network"><NetworkSection /></section>
-      <section id="projects"><ProjectsSection /></section>
+      <section id="network"><NetworkTrackerSection /></section>
+      <section id="projects"><ProjectsGrid /></section>
       <section id="experience"><ExperienceSection /></section>
-      <section id="certs"><CertsSection /></section>
+      <section id="certs"><CertificationsSection /></section>
       <section id="contact"><ContactSection /></section>
     </>
   )
